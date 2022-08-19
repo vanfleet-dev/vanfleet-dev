ad73ebfdc1006235c109d7a867118980ff6ff544
vanfleet-dev <vanfleet.dev@protonmail.com>
2022-08-18 21:53:28 -0700

    dev log 33: javascript, html, scss

---
+++ game_website/game_website.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelector('.tabs');
+
+tabs.addEventListener('click', function(e){
+    handleClick(e);
+});
+// tabs.addEventListener('click', e => handleClick(e));
+
+function handleClick(e) {
+  const target = e.target;
+  const tabNum = target.dataset.tab;
+  const activeTab = document.querySelector('.tabs .active');
+  const activeContent = document.querySelector('.content .visible');
+  const currentContent = document.querySelector(`.content__section[data-tab='${tabNum}']`);
+  
+  if (!tabNum) {
+    return;
+  }
+  
+  activeTab.classList.remove('active');
+  target.classList.add('active');
+  activeContent.classList.remove('visible');
+  currentContent.classList.add('visible');
+}
+
+
+console.log(tabs)
