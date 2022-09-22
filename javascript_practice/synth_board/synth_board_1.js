89374cbc6da6a872f345f1cbc44361f5d4f4204f
vanfleet-dev <vanfleet.dev@protonmail.com>
2022-09-21 22:44:13 -0700

    dev log 60: javascript, html, scss

--- javascript_practice/synth_board/synth_board_1.js
+++ javascript_practice/synth_board/synth_board_1.js
@@ -10,3 +10,23 @@ synthKey.addEventListener('mouseup', function(e) {
         document.querySelector(`.${synthId}-audio`).muted = true;
     }
 });
+
+
+
+// keyboard
+const numSynthBtns = document.querySelectorAll(".synth").length;
+
+document.addEventListener("keypress", function (e) {
+    keyInput = e.key;
+    for (let i = 0; i < numSynthBtns; i++) {
+        let htmlVal = document.querySelectorAll(".synth")[i].innerHTML;
+        if ( htmlVal === keyInput) {
+            let synthId = document.querySelectorAll(".synth")[i].id;
+            if (document.querySelector(`.${synthId}-audio`).muted === true) {
+                document.querySelector(`.${synthId}-audio`).muted = false;
+            } else {
+                document.querySelector(`.${synthId}-audio`).muted = true;
+            }
+        }
+    }
+});
