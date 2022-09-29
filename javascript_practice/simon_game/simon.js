e1b4bfa2d8a8868db176af7e35c923db5fa1bfa4
vanfleet-dev <vanfleet.dev@protonmail.com>
2022-09-28 21:01:01 -0700

    dev log 65: javascript, jquery

--- javascript_practice/simon_game/simon.js
+++ javascript_practice/simon_game/simon.js
@@ -3,8 +3,9 @@
 
 let patternArr = [];
 let currentPatternArr = [];
-let levelCount = 1;
-let i = 0;
+let playerChoicesArr = [];
+let levelCount = 3;
+let moveCount = 0;
 
 for (let i = 0; i < 100; i++) { // make pattern
     let num = Math.random();
@@ -27,76 +28,125 @@ console.log(`total pattern: ${patternArr}`);
 $(document).on('keypress', function (event) {
     if ( event.key === "n") {
 
-        currentPatternArr.push(patternArr[i]);
-
-        // anim ///////
-        let currentStep = patternArr[i];
-        let audio = new Audio (`./audio/${currentStep}.mp3`);
-        audio.play();
-        $(`#${currentStep}`).addClass('pressed');
-        setTimeout(function () {
-            $(`#${currentStep}`).removeClass('pressed')
-        }, 300);
-        //////////////
-
-        console.log(`current pattern: ${currentPatternArr}`);
-        console.log(`new pattern move: ${patternArr[i]}`);
-        console.log(`levelCount: ${levelCount}`);
-        console.log(`i: ${i}`);
-
-        $('.btn').on('click', function (event) {
-
-            let playerChoice = event.target.id;
-        
-            // anim ///////
-            let audio = new Audio (`./audio/${playerChoice}.mp3`); 
-            audio.play();
-            $(`#${playerChoice}`).addClass('pressed');
-            setTimeout(function () {
-                $(`#${playerChoice}`).removeClass('pressed');
-            }, 300);
-            //////////////
-
-            if (playerChoice !== currentPatternArr[i]) {
-                $("body").html("<p>GAME OVER<p/>").css("color", "#d7B550").css("font-size", "9rem"); // game over
-            }
-
+        if (currentPatternArr.length !== levelCount) {
+            for (;moveCount < levelCount ;) {
             
-            if (playerChoice === currentPatternArr[i] && i < levelCount) { 
-                i++
-                currentPatternArr.push(patternArr[i]);
-                
-                console.log(`player choice: ${playerChoice}`);
-                console.log(`new pattern added: ${patternArr[i]}`);
-                console.log(`current pattern: ${currentPatternArr}`);
-                console.log(`levelCount: ${levelCount}`);
-                console.log(`i: ${i}`);
+                currentPatternArr.push(patternArr[moveCount]);
+    
+                // let audio = new Audio (`./audio/${currentPatternArr[moveCount]}.mp3`);
+                // audio.play();
+        
+                //$(`#${currentPatternArr[moveCount]}`).addClass('pressed');
+    
+                //$(`#${currentPatternArr[moveCount]}`).removeClass('pressed');
+    
+                moveCount++;
 
             }
             
-            if (i > levelCount) { 
-                i = 0;
-                levelCoun++;
-                currentPatternArr.push(patternArr[levelCount]);
+            if (currentPatternArr.length === levelCount) {
+                moveCount = 0;
+            }
+        };        
 
-                // anim ///////
-                let currentStep = patternArr[levelCount]; 
-                let audio = new Audio (`./audio/${currentStep}.mp3`);
-                audio.play();
-                $(`#${currentStep}`).addClass('pressed');
-                setTimeout(function () {
-                $(`#${currentStep}`).removeClass('pressed')
-                }, 300);
-                //////////////
+        test1 (); //console logs
 
-                console.log(`levelCount: ${levelCount}`);
-                console.log(`i: ${i}`);
-
-            } 
-            
-
-        });
-            
-        
+        $('.btn').on('click', clickCheck);
     }
 });
+
+
+function clickCheck (e) {
+    let audio = new Audio (`./audio/${e.target.id}.mp3`); 
+    audio.play();
+    $(`#${e.target.id}`).addClass('pressed');
+    setTimeout(function () {
+        $(`#${e.target.id}`).removeClass('pressed');
+    }, 300);
+
+    if (e.target.id === currentPatternArr[moveCount]) {
+        playerChoicesArr.push(e.target.id);
+        moveCount++;
+        if (playerChoicesArr.length === currentPatternArr.length) {
+            levelCount++;
+            moveCount = 0;
+            currentPatternArr = [];
+            playerChoicesArr = [];
+        }
+        console.log(`intra move count: ${moveCount}`); //test
+        console.log(`player choices: ${playerChoicesArr}`); //test
+    } else {
+        $("body").html("<p>GAME OVER<p/>").css("color", "#d7B550").css("font-size", "9rem"); // game over
+    }
+    
+    console.log(`target ID: ${e.target.id}`); //test
+}
+
+
+function moveAmin(val) {
+    // anim ///////
+    let audio = new Audio (`./audio/${e.target.id}.mp3`); 
+    audio.play();
+    $(`#${e.target.id}`).addClass('pressed');
+    setTimeout(function () {
+        $(`#${e.target.id}`).removeClass('pressed');
+    }, 300);
+    //////////////
+};
+
+
+
+function test1 () {
+    console.log(`current pattern: ${currentPatternArr}`);
+    //console.log(`new pattern move: ${patternArr[moveCount]}`);
+    console.log(`levelCount: ${levelCount}`);
+    console.log(`move count: ${moveCount}`);
+};
+
+
+
+function test2 () {
+    console.log(`player choice: ${e.target.id}`);
+    //console.log(`new pattern added: ${patternArr[moveCount]}`);
+    console.log(`current pattern: ${currentPatternArr}`);
+    console.log(`levelCount: ${levelCount}`);
+    console.log(`move count: ${moveCount}`);
+};
+
+
+
+function moveAmin(val) {
+    // anim ///////
+    let audio = new Audio (`./audio/${e.target.id}.mp3`); 
+    audio.play();
+    $(`#${e.target.id}`).addClass('pressed');
+    setTimeout(function () {
+        $(`#${e.target.id}`).removeClass('pressed');
+    }, 300);
+    //////////////
+};
+
+
+
+(e.target.id !== currentPatternArr[moveCount]) 
+
+
+if (i > levelCount) { 
+    i = 0;
+    levelCoun++;
+    currentPatternArr.push(patternArr[levelCount]);
+
+    // anim ///////
+    let currentStep = patternArr[levelCount]; 
+    let audio = new Audio (`./audio/${currentStep}.mp3`);
+    audio.play();
+    $(`#${currentStep}`).addClass('pressed');
+    setTimeout(function () {
+    $(`#${currentStep}`).removeClass('pressed')
+    }, 300);
+    //////////////
+
+    console.log(`levelCount: ${levelCount}`);
+    console.log(`i: ${i}`);
+
+} 
